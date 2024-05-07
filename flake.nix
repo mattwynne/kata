a3ab49aab9efbee1e9c474da258fb933e0df7d52
Matt Wynne <matt.wynne@mechanical-orchard.com>
2024-05-06 21:08:50 -0700

    Flake to install elixir

--- flake.nix
+++ flake.nix
@@ -1,28 +1,33 @@
 {
-  description = "A Nix-flake-based Elixir development environment";
+  description = "Install elixir";
 
-  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
     let
-      # supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      # supportedSystems = [ "aarch64-darwin" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit system; };
-      });
+      inherit (pkgs.lib) optional optionals;
+      pkgs = import nixpkgs { inherit system; };
+
+      elixir = pkgs.beam.packages.erlang.elixir;
     in
+    with pkgs;
     {
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell {
-          packages = (with pkgs; [ elixir ]) ++
-            # Linux only
-            (pkgs.lib.optionals (pkgs.stdenv.isLinux)
-              (with pkgs; [ gigalixir inotify-tools libnotify ])) ++
-            # macOS only
-            pkgs.lib.optionals (pkgs.stdenv.isDarwin)
-              ((with pkgs; [ terminal-notifier ]) ++
-                (with pkgs.darwin.apple_sdk.frameworks; [ CoreFoundation CoreServices ]));
-        };
-      });
-    };
+      devShell = pkgs.mkShell {
+        buildInputs = [
+          elixir
+          elixir_ls
+          glibcLocales
+        ] ++ optional stdenv.isLinux inotify-tools
+          ++ optional stdenv.isDarwin terminal-notifier
+          ++ optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
+            CoreFoundation
+            CoreServices
+          ]);
+      };
+    });
+
 }
